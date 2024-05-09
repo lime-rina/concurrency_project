5108c4df3d1b0ffe8e8833520d4f8d2f2c2d4c71
lime-rina <marinayordanova@limechain.tech>
2024-05-09 16:16:13 +0300

    concurrencies added

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/price-calculator/filemanager"
+	"example.com/price-calculator/prices"
+)
+
+func main() {
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
+	doneChans := make([]chan bool, len(taxRates))
+	errorChans := make([]chan error, len(taxRates))
+	for i, taxRate := range taxRates {
+		doneChans[i] = make(chan bool)
+		errorChans[i] = make(chan error)
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		// cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+
+		go priceJob.Process(doneChans[i], errorChans[i])
+
+		// if err != nil {
+		// 	fmt.Println("Could not process job")
+		// 	fmt.Println(err)
+		// }
+	}
+
+	for index := range taxRates {
+		select {
+		case err := <-errorChans[index]:
+			if err != nil {
+				fmt.Println(err)
+			}
+		case <-doneChans[index]:
+			fmt.Println("Done")
+		}
+	}
+
+	// for _, donechan := range doneChans {
+	// 	<-donechan
+	// }
+}
